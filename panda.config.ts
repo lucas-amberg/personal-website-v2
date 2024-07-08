127574fc8169b8b1a77dfa3caa553438c54ea6ba
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-07-08 10:22:27 -0700

    feat(ui): update panda.config.ts

--- panda.config.ts
+++ panda.config.ts
@@ -1,20 +1,40 @@
 import { defineConfig } from "@pandacss/dev";
+import radixColorsPreset from "pandacss-preset-radix-colors";
 
 export default defineConfig({
-  // Whether to use css reset
-  preflight: true,
+    // Whether to use css reset
+    preflight: true,
 
-  // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+    presets: [
+        "@pandacss/preset-base",
+        radixColorsPreset(),
+    ],
+    // Where to look for your css declarations
+    include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
 
-  // Files to exclude
-  exclude: [],
+    // Files to exclude
+    exclude: [],
+    prefix: "panda",
+    layers: {
+        reset: "panda_reset",
+        base: "panda_base",
+        tokens: "panda_tokens",
+        recipes: "panda_recipes",
+        utilities: "panda_utilities",
+    },
 
-  // Useful for theme customization
-  theme: {
-    extend: {},
-  },
+    // Useful for theme customization
+    theme: {
+        extend: {
+            tokens: {
+                fonts: {
+                  // add fonts later
+                },
+            },
+        },
+    },
 
-  // The output directory for your css system
-  outdir: "styled-system",
+    jsxFramework: "react",
+    // The output directory for your css system
+    outdir: "styled-system",
 });
