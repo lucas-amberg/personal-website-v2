69e2707e20b7e9ec3debf66366f4b0419a452414
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-07-08 10:29:18 -0700

    lint: run prettier with new rules

--- panda.config.ts
+++ panda.config.ts
@@ -2,36 +2,36 @@ import { defineConfig } from "@pandacss/dev";
 import radixColorsPreset from "pandacss-preset-radix-colors";
 
 export default defineConfig({
-  // Whether to use css reset
-  preflight: true,
+    // Whether to use css reset
+    preflight: true,
 
-  presets: ["@pandacss/preset-base", radixColorsPreset()],
-  // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+    presets: ["@pandacss/preset-base", radixColorsPreset()],
+    // Where to look for your css declarations
+    include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
 
-  // Files to exclude
-  exclude: [],
-  prefix: "panda",
-  layers: {
-    reset: "panda_reset",
-    base: "panda_base",
-    tokens: "panda_tokens",
-    recipes: "panda_recipes",
-    utilities: "panda_utilities",
-  },
-
-  // Useful for theme customization
-  theme: {
-    extend: {
-      tokens: {
-        fonts: {
-          // add fonts later
-        },
-      },
+    // Files to exclude
+    exclude: [],
+    prefix: "panda",
+    layers: {
+        reset: "panda_reset",
+        base: "panda_base",
+        tokens: "panda_tokens",
+        recipes: "panda_recipes",
+        utilities: "panda_utilities",
     },
-  },
 
-  jsxFramework: "react",
-  // The output directory for your css system
-  outdir: "styled-system",
+    // Useful for theme customization
+    theme: {
+        extend: {
+            tokens: {
+                fonts: {
+                    // add fonts later
+                },
+            },
+        },
+    },
+
+    jsxFramework: "react",
+    // The output directory for your css system
+    outdir: "styled-system",
 });
