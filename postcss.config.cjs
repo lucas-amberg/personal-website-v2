4245e9c9af0c9c27b818884f88affc892fe765d1
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-07-08 10:24:10 -0700

    feat(ui): update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: {
-    '@pandacss/dev/postcss': {},
+    "@pandacss/dev/postcss": {},
+    autoprefixer: {},
+    "postcss-import": {}, // This guarantees import order of css files for nextjs. Required for panda to work.
   },
-}
+};
