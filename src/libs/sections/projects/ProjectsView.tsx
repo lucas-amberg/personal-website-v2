b77f36fa6a95e856183a8146cc723cb11ce3c59b
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-08-21 21:03:19 -0700

    prettier

--- src/libs/sections/projects/ProjectsView.tsx
+++ src/libs/sections/projects/ProjectsView.tsx
@@ -111,7 +111,7 @@ export function ProjectsView() {
                 }}>
                 <Fade triggerOnce>
                     <ProjectItem
-                        href='https://www.interngoat.com'
+                        href="https://www.interngoat.com"
                         images={[
                             {
                                 src: "/projects/interngoat-homepage.png",
@@ -127,20 +127,20 @@ export function ProjectsView() {
                             },
                         ]}
                         title="Interngoat"
-                        description="Lead engineer of Interngoat, building the best platform to find internships worldwide."
+                        description="Co-founder of Interngoat, building the best platform to find internships worldwide."
                         skills={[
                             "Next.js",
                             "TypeScript",
                             "Panda CSS",
                             "Neo4j",
                             "Web Scraping",
-                            "Open AI"
+                            "Open AI",
                         ]}
                         startDate="August 2024"
                         endDate="In Progress"
                         tasks={[
                             "Built autonomous web scraping system to find and input internships worldwide, adding hundreds of internships.",
-                            "Developed the full platform including an internship list and search feature, company list, and newsletter (coming soon)."
+                            "Developed the full platform including an internship list and search feature, company list, and newsletter (coming soon).",
                         ]}
                     />
                 </Fade>
